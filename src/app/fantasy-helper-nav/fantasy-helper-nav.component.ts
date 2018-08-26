fcd20f25cacaa96bd8e2f3224ab67b57ef6a47f7
MacNew <newmac1000@gmail.com>
2018-08-26 07:47:09 +0545

    Log out complet

--- src/app/fantasy-helper-nav/fantasy-helper-nav.component.ts
+++ src/app/fantasy-helper-nav/fantasy-helper-nav.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-fantasy-helper-nav',
@@ -15,6 +16,15 @@ export class FantasyHelperNavComponent {
       map(result => result.matches)
     );
     
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private router: Router
+  ) {}
   
+  logout() {
+    localStorage.removeItem('token');
+    this.router.navigate(['/']);
+   }
   }
+
+
