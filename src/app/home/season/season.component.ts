933a6b125747ca85a3f9123478f028c2f8b8e11f
MacNew <newmac1000@gmail.com>
2018-09-25 23:50:25 +0545

    #66-create-season-menu in progress

--- src/app/home/season/season.component.ts
+++ src/app/home/season/season.component.ts
@@ -1,17 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { SpringService } from '../../share/springService/spring.service'
-
+import { FormGroup, Validators,FormBuilder, AbstractControl } from '@angular/forms';
+import { CustomValidators } from '../../validators/custom-validators';
 
 @Component({
   selector: 'app-season-first',
   templateUrl: './season.component.html',
   styleUrls: ['./season.component.css']
 })
+
 export class Season implements OnInit {
   clublist: clubDetails[] = [];
   forwardPlayerlist: playerDetails[] = [];
-  midfilderPlayerlist: playerDetails[] = []; 
-  constructor(private springService: SpringService) { 
+  midfilderPlayerlist: playerDetails[] = [];
+  defenderPlayerlist: playerDetails[] = [];
+  goalkeeperslist: playerDetails[] = [];
+  private seasonForm:FormGroup;
+  mySeasonForm = {
+    seasonName: ['', Validators.required],
+    winerClub: ['', Validators.required, CustomValidators.matchClubsecond ],
+    runnerUpClub: ['', Validators.required, CustomValidators.matchClub]  
+  }
+  constructor(
+    private springService: SpringService,
+    private formBilder: FormBuilder
+    ) {
+    this.seasonForm = this.formBilder.group(this.mySeasonForm);
   }
 
   ngOnInit() {
@@ -21,6 +35,25 @@ export class Season implements OnInit {
         this.forwardPlayerlist.push(Object.assign({playerName: element.playerName, id:element.id}));
       });
     });
+
+    this.getPlayerlist('Midfielder').subscribe((data: any)=>{
+      data.forEach(element => {
+        this.midfilderPlayerlist.push(Object.assign({playerName: element.playerName, id:element.id}));
+      });
+    });
+
+    this.getPlayerlist('Defender').subscribe((data: any)=>{
+      data.forEach(element => {
+        this.defenderPlayerlist.push(Object.assign({playerName: element.playerName, id:element.id}));
+      });
+    });
+
+    this.getPlayerlist('GoalKeeper').subscribe((data: any)=>{
+      data.forEach(element => {
+        this.goalkeeperslist.push(Object.assign({playerName: element.playerName, id:element.id}));
+      });
+    });
+
   }
 
   assignedClubName() {
